3cda757f4b619fae727dbc231ad1cf44bc84c5c0
ancythomas1894-ship-it <ancythomas1894@gmail.com>
2026-08-25 00:26:02 +0530

    Add files via upload

--- employee_assignment.sql
+++ employee_assignment.sql
@@ -1,1 +1,76 @@
+DROP DATABASE IF EXISTS employee;
+CREATE DATABASE employe;
+USE employee;
 
+CREATE TABLE Departments (
+    department_id INT PRIMARY KEY,
+    department_name VARCHAR(100) NOT NULL UNIQUE
+) ENGINE=InnoDB;
+
+CREATE TABLE Location (
+    location_id INT PRIMARY KEY AUTO_INCREMENT,
+    location VARCHAR(30) NOT NULL UNIQUE
+) ENGINE=InnoDB;
+
+CREATE TABLE Employees (
+    employee_id INT PRIMARY KEY,
+    employee_name VARCHAR(50) NOT NULL,
+    gender ENUM('M','F'),
+    age INT,
+    hire_date DATE DEFAULT (CURRENT_DATE),
+    designation VARCHAR(100),
+    department_id INT,
+    location_id INT,
+    salary DECIMAL(10,2),
+    CONSTRAINT fk_department
+        FOREIGN KEY (department_id)
+        REFERENCES Departments(department_id),
+    CONSTRAINT fk_location
+        FOREIGN KEY (location_id)
+        REFERENCES Location(location_id),
+    CONSTRAINT chk_age
+        CHECK (age >= 18)
+) ENGINE=InnoDB;
+ALTER TABLE Employees
+ADD email VARCHAR(100);
+ALTER TABLE Employees
+MODIFY designation VARCHAR(200);
+ALTER TABLE Employees
+DROP COLUMN age;
+ALTER TABLE Employees
+RENAME COLUMN hire_date TO date_of_joining;
+RENAME TABLE Departments TO Departments_Info;
+RENAME TABLE Location TO Locations;
+TRUNCATE TABLE Employees;
+DROP TABLE Emplooyes;
+
+DROP DATABASE IF EXISTS employee;
+CREATE DATABASE employe;
+USE employee;
+
+CREATE TABLE Departments(
+department_id INT PRIMARY KEY,
+    department_name VARCHAR(100) NOT NULL UNIQUE);
+  CREATE TABLE Location (
+    location_id INT PRIMARY KEY AUTO_INCREMENT,
+    location VARCHAR(30) NOT NULL UNIQUE);
+    
+CREATE TABLE Employees (
+    employee_id INT PRIMARY KEY,
+    employee_name VARCHAR(50) NOT NULL,
+    gender ENUM('M','F'),
+    age INT CHECK (age >= 18),
+    hire_date DATE DEFAULT (CURRENT_DATE),
+    designation VARCHAR(100),
+    department_id INT,
+    location_id INT,
+    salary DECIMAL(10,2),
+		FOREIGN KEY (department_id)
+        REFERENCES Departments(department_id),
+    FOREIGN KEY (location_id)
+        REFERENCES Location(location_id));
+        
+SHOW TABLES;
+ 
+    
+    
